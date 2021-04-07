d517bb71ce0256b84a66b34e2a4a3a3c7b7d4db3
Zakharov <i.zaharov98@mail.ru>
2021-04-07 19:24:36 +0300

    нечеткая страница авторизации

--- lib/Views/login_view.dart
+++ lib/Views/login_view.dart
@@ -1,21 +1,15 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:picturn/ViewModels/login_view_model.dart';
 import 'navigation_bar_view.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(body: Body());
-  }
+  _LoginPage createState() => _LoginPage();
 }
 
-class Body extends StatefulWidget {
-  @override
-  _BodyState createState() => _BodyState();
-}
-
-class _BodyState extends State<Body> {
+class _LoginPage extends State<LoginPage> {
   LoginViewModel loginViewModel = LoginViewModel();
 
   @override
@@ -26,23 +20,23 @@ class _BodyState extends State<Body> {
 
   void click() {
     this.loginViewModel.signInWithGoogle().then(
-      (_) => {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => NavigationBarView(),
-          ),
-        ),
-      },
-    );
+          (_) => {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => NavigationBarView(),
+              ),
+            ),
+          },
+        );
   }
 
   Widget googleLoginButton() {
     return OutlineButton(
       onPressed: this.click,
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(45)),
-      splashColor: Colors.grey,
-      borderSide: BorderSide(color: Colors.grey),
+      splashColor: Colors.indigo[400],
+      borderSide: BorderSide(color: Colors.black),
       child: Padding(
         padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
         child: Row(
@@ -51,9 +45,12 @@ class _BodyState extends State<Body> {
           children: <Widget>[
             Image(image: AssetImage('res/images/google_logo.png'), height: 35),
             Padding(
-                padding: EdgeInsets.only(left: 10),
-                child: Text('Sign in with Google',
-                    style: TextStyle(color: Colors.grey, fontSize: 25)))
+              padding: EdgeInsets.only(left: 10),
+              child: Text(
+                'Sign in with Google',
+                style: TextStyle(color: Colors.indigo, fontSize: 17),
+              ),
+            ),
           ],
         ),
       ),
@@ -62,6 +59,88 @@ class _BodyState extends State<Body> {
 
   @override
   Widget build(BuildContext context) {
-    return Align(alignment: Alignment.center, child: googleLoginButton());
+    //final bottom = 282.28;
+    final bottom = MediaQuery.of(context).viewInsets.bottom;
+    print(bottom.toString());
+    double height = MediaQuery.of(context).size.height;
+    double dx = bottom - height / 5;
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: SingleChildScrollView(
+        reverse: true,
+        child: Padding(
+          padding: EdgeInsets.only(bottom: bottom),
+          child: Column(
+            children: <Widget>[
+              Container(
+                // decoration: BoxDecoration(
+                //     border: Border.all(color: Colors.green, width: 2)),
+                alignment: Alignment.center,
+                padding: EdgeInsets.fromLTRB(0, height * 0.2 , 0, height * 0.05),
+                child: Text(
+                  'Picturn',
+                  style: TextStyle(
+                    fontFamily: 'RammettoOne',
+                    color: Colors.indigo,
+                    fontSize: 50,
+                  ),
+                ),
+              ),
+              Container(
+                // decoration: BoxDecoration(
+                //     border: Border.all(color: Colors.red, width: 2)),
+                height: height * 0.05,
+              ),
+              googleLoginButton(),
+              Container(
+                // decoration: BoxDecoration(
+                //     border: Border.all(color: Colors.red, width: 2)),
+                height: height * 0.05,
+              ),
+              Column(
+                children: [
+                  FractionallySizedBox(
+                    alignment: Alignment.center,
+                    widthFactor: 0.8,
+                    child: TextField(
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Login',
+                          isDense: true,
+                          contentPadding: EdgeInsets.all(13.5)),
+                    ),
+                  ),
+                  Container(
+                    // decoration: BoxDecoration(
+                    //     border: Border.all(color: Colors.red, width: 2)),
+                    height: height * 0.01,
+                  ),
+                  FractionallySizedBox(
+                    alignment: Alignment.center,
+                    widthFactor: 0.8,
+                    child: TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Password',
+                          isDense: true,
+                          contentPadding: EdgeInsets.all(13.5)),
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(Colors.indigo)),
+                    child: Text('      Sign in      '),
+                  ),
+                ],
+              ),
+
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
